f0383bb0671bbf8dd83eb72ffb58ac5751f2cd6a
Charles Keyes <230432424+SoulSaga1106@users.noreply.github.com'pwdgit config --global user.email 230432424+SoulSaga1106@users.noreply.github.com>
2026-03-20 19:36:40 -0400

    removed uppercase letter from s3 name

--- test-bucket.tf
+++ test-bucket.tf
@@ -5,25 +5,25 @@ terraform {
       version = "~> 5.0" # Use latest version if possible
     }
   }
-
-  backend "s3" {
-    bucket  = "terraform-state-aaronmcd"                 # Name of the S3 bucket
-    key     = "jenkins-test-031726.tfstate"                               # The name of the state file in the bucket
-    region  = "us-east-2"                                                 # Use a variable for the region
-    encrypt = true                                                        # Enable server-side encryption (optional but recommended)
-  } 
 }
+#   backend "s3" {
+#     bucket  = "terraform-state-aaronmcd"                 # Name of the S3 bucket
+#     key     = "jenkins-test-031726.tfstate"                               # The name of the state file in the bucket
+#     region  = "us-east-2"                                                 # Use a variable for the region
+#     encrypt = true                                                        # Enable server-side encryption (optional but recommended)
+#   } 
+# }
 
 provider "aws" {
   region  = "us-east-2"
 }
 
 resource "aws_s3_bucket" "frontend" {
-  bucket_prefix = "jenkins-bucket-"
+  bucket_prefix = "wutang-bucket-"
   force_destroy = true
   
 
   tags = {
-    Name = "Jenkins Bucket"
+    Name = "wutang-bucket"
   }
 }
